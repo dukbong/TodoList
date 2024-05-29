e421324db5dd60e8a931ab71fb33dc5486543ab8
JANG <gorae930301@gmail.com>
2024-05-30 00:00:04 +0900

    토큰 기반 인증 세팅 초기

---
+++ src/main/java/com/example/todolist/security/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.example.todolist.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.todolist.security.filter.JWTFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public JWTFilter jwtFilter() {
+		return new JWTFilter();
+	}
+	
+	@Bean
+	public AccessDeniedHandler accessDeniedHandler() {
+		return null;
+	}
+	
+	@Bean
+	public AuthenticationEntryPoint authenticationEntryPoint() {
+		return null;
+	}
+	
+	@Bean
+	public RoleHierarchy roleHierarchy() {
+		return RoleHierarchyImpl.fromHierarchy("ROLE_ADMIN > ROLE_USER");
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> {
+			auth.requestMatchers("/", "/login", "/join").permitAll();
+			auth.requestMatchers("/todo/**").hasRole("USER");
+			auth.requestMatchers("/log/**").hasRole("ADMIN");
+			auth.anyRequest().authenticated();
+		});
+		
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		http.csrf(csrf -> csrf.disable()); // 명시적 작성
+		http.formLogin(login -> login.disable());
+		http.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+		http.exceptionHandling(ex -> ex.accessDeniedHandler(accessDeniedHandler()).authenticationEntryPoint(authenticationEntryPoint()));
+		
+		return http.build();
+	}
+}
---
+++ src/main/java/com/example/todolist/security/handler/JWTAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.example.todolist.security.handler;
+
+import java.io.IOException;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JWTAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException, ServletException {
+		
+	}
+
+}
